ef60469b577f719441fb7c127bf67f4c25032633
RyanL72 <105567147+RyanL72@users.noreply.github.com>
2024-10-06 01:29:31 -0400

    began

---
+++ Project_2/proj2.cpp
@@ -0,0 +1,15 @@
+/*
+Author: Ryan Lin
+CaseID: rhl72
+File name: proj2.cpp
+Date: 10-16-2024
+Description: Web Client
+*/
+
+#include <string>
+#include <cstdlib>
+#include <iostream>
+
+// Define constants
+const std::string URLHEADING = "http://";
+

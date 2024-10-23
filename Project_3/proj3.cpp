a6056c1736cf850dee6910d2a5c98c0e3ae33a9a
RyanL72 <105567147+RyanL72@users.noreply.github.com>
2024-10-23 15:54:14 -0400

    i can check commands

--- Project_3/proj3.cpp
+++ Project_3/proj3.cpp
@@ -16,6 +16,10 @@ Description: IPv4 format checker
 #include <netinet/in.h>
 
 #include <iostream>
+#include <cstdlib>   // for exit()
+#include <string>    // for std::string
+#include <cctype>    // for isdigit
+#include <stdexcept> // for std::stoi
 
 #define REQUIRED_ARGC 6
 #define PORT_POS 1
@@ -24,16 +28,13 @@ Description: IPv4 format checker
 #define QLEN 1
 #define PROTOCOL "tcp"
 #define BUFLEN 1024
-#define PORT_NUMBER_POS -1
-#define ROOT_DIRECTORY_POS -1 
-#define TERMINATION_TOKEN_POS -1
 
-
-int usage (char *progname)
+int usage (std::string progname)
 {
-    fprintf (stderr,"usage: %s port msg\n", progname);
+    std::cout << progname << " is an unknown option, please use the format: \n" <<  "./proj3 -p <Port Number> -r <Root directory> -t <Terminate token>" << std::endl;
     exit (ERROR);
 }
+
 
 int errexit (std::string format, std::string arg)
 {
@@ -42,88 +43,87 @@ int errexit (std::string format, std::string arg)
     exit (ERROR);
 }
 
+int main (int argc, char *argv [])
+{
+    //for ipv4
+    struct sockaddr_in sin;
+    
+    //address for ipv4 or ipv6
+    struct sockaddr addr;
 
+    //protocal information 
+    struct protoent *protoinfo;
 
-void parseArgs(int argc, char *argv []){
-    try{
-        for(int i = 1; i < argc; i++){
-            switch(argv [i]){
-                case "-p":
-                    i++;
-                    PORT_NUMBER_POS = i;
+    //length of address
+    unsigned int addrlen;
+
+    //socket descriptors 
+    int sd, sd2;
+    
+    //check to see if the right options were provided.
+        int portNumber;
+        std::string rootDirectory;
+        int terminateToken;
+
+        //get the options.
+        int opt;
+        while((opt = getopt(argc, argv,"p:r:t:")) != -1){
+            switch(opt){
+                case 'p':
+                    portNumber = atoi(optarg);
                     break;
-                case "-r":
-                    i++;
-                    ROOT_DIRECTORY_POS = i;
+                case 'r':   
+                    rootDirectory = optarg;
                     break;
-                case "-t":
-                    i++;
-                    TERMINATION_TOKEN_POS = i;
+                case 't':
+                    terminateToken = atoi(optarg);
+                    break;
+                case '?':
+                    usage(std::to_string(opt));
                     break;
             }
         }
-    }
-    catch(const char* msg){
-        std::cout << "bruh" << msg;
-    }
-}
-
-int main (int argc, char *argv [])
-{
-    struct sockaddr_in sin;
-    struct sockaddr addr;
-    struct protoent *protoinfo;
-    unsigned int addrlen;
-    int sd, sd2;
-    
-    if (argc != REQUIRED_ARGC)
-        usage (argv [0]);
-    
-    parseArgs(argc, argv);
-    std::cout << PORT_NUMBER_POS << std::endl;
-    std::cout << ROOT_DIRECTORY_POS << std::endl;
-    std::cout << TERMINATION_TOKEN_POS << std::endl;
-
-    // for(int i = 0; i < argc; i++){
-    //     std::cout << argv[i] << "\n";
-    // }
+        //check to see if all the port, root directory, and terminate token were provided
+        std::cout << portNumber << std::endl;
+        std::cout << rootDirectory << std::endl;
+        std::cout <<terminateToken << std::endl;
 
     /* determine protocol */
-    // if ((protoinfo = getprotobyname (PROTOCOL)) == NULL)
-    //     errexit ("cannot find protocol information for ", PROTOCOL);
+    if ((protoinfo = getprotobyname (PROTOCOL)) == NULL)
+        errexit ("cannot find protocol information for ", PROTOCOL);
 
-    // /* setup endpoint info */
-    // memset ((char *)&sin,0x0,sizeof (sin));
-    // sin.sin_family = AF_INET;
-    // sin.sin_addr.s_addr = INADDR_ANY;
-    // sin.sin_port = htons ((u_short) atoi (argv [PORT_POS]));
+    /* setup endpoint info */
+    memset ((char *)&sin,0x0,sizeof (sin));
+    sin.sin_family = AF_INET;
+    sin.sin_addr.s_addr = INADDR_ANY;
+    sin.sin_port = htons ((u_short) atoi (argv [PORT_POS]));
 
-    // /* allocate a socket */
-    // /*   would be SOCK_DGRAM for UDP */
-    // sd = socket(PF_INET, SOCK_STREAM, protoinfo->p_proto);
-    // if (sd < 0)
-    //     errexit("cannot create socket", NULL);
+    /* allocate a socket */
+    /*   would be SOCK_DGRAM for UDP */
+    sd = socket(PF_INET, SOCK_STREAM, protoinfo->p_proto);
+    if (sd < 0)
+        errexit("cannot create socket", NULL);
 
-    // /* bind the socket */
-    // if (bind (sd, (struct sockaddr *)&sin, sizeof(sin)) < 0)
-    //     errexit ("cannot bind to port %s", argv [PORT_POS]);
+    /* bind the socket */
+    if (bind (sd, (struct sockaddr *)&sin, sizeof(sin)) < 0)
+        errexit ("cannot bind to port %s", argv [PORT_POS]);
 
-    // /* listen for incoming connections */
-    // if (listen (sd, QLEN) < 0)
-    //     errexit ("cannot listen on port %s\n", argv [PORT_POS]);
+    /* listen for incoming connections */
+    if (listen (sd, QLEN) < 0)
+        errexit ("cannot listen on port %s\n", argv [PORT_POS]);
 
-    // /* accept a connection */
-    // addrlen = sizeof (addr);
-    // sd2 = accept (sd,&addr,&addrlen);
-    // if (sd2 < 0)
-    //     errexit ("error accepting connection", NULL);
+    /* accept a connection */
+    addrlen = sizeof (addr);
+    sd2 = accept (sd,&addr,&addrlen);
+    if (sd2 < 0)
+        errexit ("error accepting connection", NULL);
     
-    // /* write message to the connection */
-    // if (write (sd2,argv [MSG_POS],strlen (argv [MSG_POS])) < 0)
-    //     errexit ("error writing message: %s", argv [MSG_POS]);
+    /* write message to the connection */
+    if (write (sd2,argv [MSG_POS],strlen (argv [MSG_POS])) < 0)
+        errexit ("error writing message: %s", argv [MSG_POS]);
 
-    // /* close connections and exit */
-    // close (sd);
-    // close (sd2);
+    /* close connections and exit */
+    close (sd);
+    close (sd2);
     exit (0);
 }
